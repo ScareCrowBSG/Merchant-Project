f10f03c3fd4c389b16f256e23dc21476d87bca7c
darkmushroom <mensing.alexander@gmail.com>
2018-12-02 21:44:22 -0500

    Added .gitignore and base source code

---
+++ main.cs
@@ -0,0 +1,12 @@
+using System;
+
+class merchant_game 
+{
+    static void Main() 
+    {
+        Console.WriteLine("Howdy!");
+        // Keep the console window open in debug mode.
+        Console.WriteLine("Press any key to exit.");
+        Console.ReadKey();
+    }
+}
