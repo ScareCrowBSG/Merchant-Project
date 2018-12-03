b30160e77a90ea243c26a986954bf39b548491b0
darkmushroom <mensing.alexander@gmail.com>
2018-12-03 14:37:14 -0500

    Introduced the concept of towns and added some testing

--- main.cs
+++ main.cs
@@ -1,12 +1,58 @@
 using System;
+using System.Collections.Generic;
 
-class merchant_game 
+namespace MerchantGame
 {
-    static void Main() 
+    struct Value
     {
-        Console.WriteLine("Howdy!");
-        // Keep the console window open in debug mode.
-        Console.WriteLine("Press any key to exit.");
-        Console.ReadKey();
+        public int amount;
+        public float buyPrice;
+        public float sellPrice;
+    }
+
+    struct Town
+    {
+        public string name;
+        public Dictionary<string, Value> inventory;
+
+        public Town (string theTownName)
+        {
+            name = theTownName;
+            inventory = new Dictionary<string, Value>();
+        }
+    }
+
+    class MechantGameMain 
+    {
+        static void Main(string[] args) 
+        {
+            Console.WriteLine("\nHowdy!");
+
+            Town Pittsburgh = new Town("Pittsburgh");
+            Pittsburgh.inventory.Add("steel", new Value {amount = 10, buyPrice = 2.0f, sellPrice = 1.0f});
+
+            SlowType("We've just arrived at a town.");
+            SlowType(Pittsburgh.name + "\'s the name.");
+            
+            foreach(KeyValuePair<string, Value> entry in Pittsburgh.inventory)
+            {
+
+                SlowType("I see this town has " + entry.Value.amount + " " + entry.Key + ".");
+                SlowType("I reckon we could sell it for about $" + entry.Value.sellPrice);
+                SlowType("..but it's going to be $" + entry.Value.buyPrice + " to buy it.");
+
+            }
+            
+            System.Threading.Thread.Sleep(5000);
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+
+        static void SlowType(string output)
+        {
+            Console.WriteLine(output);
+            System.Threading.Thread.Sleep(3000);
+        }
     }
 }
+
