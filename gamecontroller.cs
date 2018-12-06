3d5ebd107107517ee5fa5bc6901cdb0940ae91a3
darkmushroom <mensing.alexander@gmail.com>
2018-12-05 19:48:00 -0500

    Experimenting with partial declarations

---
+++ gamecontroller.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+
+namespace MerchantGame
+{
+	public partial class MerchantGame
+	{
+		public void start()
+		{
+			Console.WriteLine("We did it!");
+		}
+		
+	}
+}
--- main.cs
+++ main.cs
@@ -3,95 +3,14 @@ using System.Collections.Generic;
 
 namespace MerchantGame
 {
+	class MerchantGameMain
+	{
+		static void Main(string[] args)
+		{
+			MerchantGame MG = new MerchantGame();
+			MG.start();
+		}
+	}
 
-    struct Pricing
-    {
-        public int amount;
-        public float buyPrice;
-        public float sellPrice;
-    }
-
-    struct Town
-    {
-        public string name;
-        public Dictionary<string, Pricing> inventory;
-
-        public Town (string theTownName)
-        {
-            name = theTownName;
-            inventory = new Dictionary<string, Pricing>();
-        }
-    }
-
-    class MerchantGame
-    {
-        public List<Town> cities;
-
-        void start() 
-        {
-            cities = new List<Town>();
-
-            AddTestCities();
-            
-
-
-            RunIntegrationTest();
-            //we're done here   
-            System.Threading.Thread.Sleep(5000);
-            Console.WriteLine("Press any key to exit.");
-            Console.ReadKey();
-        }
-
-        static void SlowType(string output)
-        {
-            Console.WriteLine(output);
-            System.Threading.Thread.Sleep(1000);
-        }
-
-        void RunIntegrationTest()
-        {
-            SlowType("\nHowdy!");
-            foreach (Town location in cities)
-            {
-                
-                SlowType("We've just arrived at a town.");
-                SlowType(location.name + "\'s the name.");
-                
-                foreach(KeyValuePair<string, Pricing> entry in location.inventory)
-                {
-                    SlowType("I see this town has " + entry.Value.amount + " " + entry.Key + ".");
-                    SlowType("I reckon we could sell for about $" + entry.Value.sellPrice);
-                    SlowType("..but it's going to be $" + entry.Value.buyPrice + " to buy it.\n");
-                }
-            }
-        }
-
-        void AddTestCities()
-        {
-            Town Pittsburgh = new Town("Pittsburgh");
-            Pittsburgh.inventory.Add("steel", new Pricing {amount = 10, buyPrice = 20.0f, sellPrice = 10.0f});
-            Pittsburgh.inventory.Add("food", new Pricing {amount = 5, buyPrice = 10.0f, sellPrice = 5.0f});
-            Pittsburgh.inventory.Add("water", new Pricing {amount = 7, buyPrice = 5.0f, sellPrice = 2.0f});
-            Pittsburgh.inventory.Add("guns", new Pricing {amount = 10, buyPrice = 75.0f, sellPrice = 25.0f});
-            cities.Add(Pittsburgh);
-
-            Town Minneapolis = new Town("Minneapolis");
-            Minneapolis.inventory.Add("guns", new Pricing {amount = 3, buyPrice = 50.0f, sellPrice = 20.0f});
-            cities.Add(Minneapolis);
-
-            Town Omaha = new Town("Omaha");
-            Omaha.inventory.Add("food", new Pricing {amount = 50, buyPrice = 2.0f, sellPrice = 1.0f});
-            cities.Add(Omaha);
-            
-        }
-
-        class MerchantGameMain
-        {
-            static void Main(string[] args)
-            {
-                MerchantGame MG = new MerchantGame();
-                MG.start();
-            }
-        }
-    }
+	public partial class MerchantGame {	}
 }
