e929e5301be87a9039919ed6301da22668537e3d
darkmushroom <mensing.alexander@gmail.com>
2018-12-05 22:10:00 -0500

    Live input and updating game clock, but I hate it

--- gamecontroller.cs
+++ gamecontroller.cs
@@ -11,17 +11,26 @@ namespace MerchantGame
         public void start()
         {
             time = new DateTime();
+            char input = '0';
 
             while(!stop)
             {
                 deltaTime();
-                Console.WriteLine(elapsed);
-                System.Threading.Thread.Sleep(1000);
+                Console.Clear();
+                Console.WriteLine("Howdy Boss. We've been up: " + elapsed + " seconds.");
+                Console.WriteLine("Go ahead and press \'x\' to leave.");
 
-                if (elapsed > 50)
+                if (Console.KeyAvailable)
+                {
+                    input = getInput();
+                }
+                
+                Console.WriteLine(input);
+                if (input == 'x')
                 {
                     stop = true;
                 }
+                System.Threading.Thread.Sleep(1000);
             }
             
         }
@@ -30,6 +39,11 @@ namespace MerchantGame
         {
             elapsed += (ulong)DateTime.Now.Subtract(time).Seconds;
             time = DateTime.Now;
+        }
+
+        public char getInput()
+        {
+            return Console.ReadKey(true).KeyChar;
         }
 
     }
