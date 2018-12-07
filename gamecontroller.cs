c042a82540e2d02ad75d113be841cdaace49fb6c
darkmushroom <mensing.alexander@gmail.com>
2018-12-06 23:03:38 -0500

    minor style change, only display owned items at base

--- gamecontroller.cs
+++ gamecontroller.cs
@@ -70,10 +70,13 @@ namespace MerchantGame
                 Console.WriteLine("Here's where we stand with our inventory right now:\n");
                 foreach(KeyValuePair<string,int> entry in theBase.inventory)
                 {
-                    Console.WriteLine(UppercaseFirst(entry.Key));
-                    Console.WriteLine("Amt: " + entry.Value 
-                        + " Buy Price: " + theBase.prices[entry.Key].buyPrice 
-                        + " Sell Price: " + theBase.prices[entry.Key].sellPrice + "\n");
+                    if (entry.Value != 0)
+                    {
+                        Console.WriteLine(UppercaseFirst(entry.Key));
+                        Console.WriteLine("Amt: " + entry.Value 
+                            + " Buy Price: " + theBase.prices[entry.Key].buyPrice 
+                            + " Sell Price: " + theBase.prices[entry.Key].sellPrice + "\n");
+                    }
                 }
                 Console.WriteLine("Press \'x\' to return to the shop");
                 input = getInput();
@@ -127,50 +130,51 @@ namespace MerchantGame
             Base.AddItem("food", 5);
             Base.AddItem("water", 3);
             Base.AddItem("guns", 1);
-			Base.AddItem("steel", 0);
-			Base.AddItem("iron", 0);
-			Base.AddItem("coal", 0);
-			Base.AddItem("alcohol", 3);
+            Base.AddItem("steel", 0);
+            Base.AddItem("iron", 0);
+            Base.AddItem("coal", 0);
+            Base.AddItem("alcohol", 3);
             cities.Add("Base", Base);
 
             Town Pittsburgh = new Town();
             Pittsburgh.AddItem("food", 5, 10.0f, 5.0f);
             Pittsburgh.AddItem("water", 7, 5.0f, 2.0f);
             Pittsburgh.AddItem("guns", 10, 75.0f, 25.0f);
-			Pittsburgh.AddItem("steel", 10, 20.0f, 10.0f);
-			Pittsburgh.AddItem("iron", 0, 50.0f, 10.0f);
-			Pittsburgh.AddItem("coal", 0, 50.0f, 10.0f);
-			Pittsburgh.AddItem("alcohol", 0, 100.0f, 25.0f);
+            Pittsburgh.AddItem("steel", 10, 20.0f, 10.0f);
+            Pittsburgh.AddItem("iron", 0, 50.0f, 10.0f);
+            Pittsburgh.AddItem("coal", 0, 50.0f, 10.0f);
+            Pittsburgh.AddItem("alcohol", 0, 100.0f, 25.0f);
             cities.Add("Pittsburgh", Pittsburgh);
 
             Town Minneapolis = new Town();
             Minneapolis.AddItem("food", 5, 10.0f, 5.0f);
             Minneapolis.AddItem("water", 7, 5.0f, 2.0f);
             Minneapolis.AddItem("guns", 10, 25.0f, 10.0f);
-			Minneapolis.AddItem("steel", 0, 20.0f, 10.0f);
-			Minneapolis.AddItem("iron", 10, 20.0f, 5.0f);
-			Minneapolis.AddItem("coal", 0, 30.0f, 10.0f);
-			Minneapolis.AddItem("alcohol", 0, 100.0f, 25.0f);
+            Minneapolis.AddItem("steel", 0, 20.0f, 10.0f);
+            Minneapolis.AddItem("iron", 10, 20.0f, 5.0f);
+            Minneapolis.AddItem("coal", 0, 30.0f, 10.0f);
+            Minneapolis.AddItem("alcohol", 0, 100.0f, 25.0f);
             cities.Add("Minneapolis", Minneapolis);
 
             Town Omaha = new Town();
             Omaha.AddItem("food", 25, 5.0f, 2.0f);
             Omaha.AddItem("water", 7, 5.0f, 2.0f);
             Omaha.AddItem("guns", 0, 100.0f, 25.0f);
-			Omaha.AddItem("steel", 0, 20.0f, 10.0f);
-			Omaha.AddItem("iron", 0, 50.0f, 20.0f);
-			Omaha.AddItem("coal", 0, 40.0f, 10.0f);
-			Omaha.AddItem("alcohol", 0, 100.0f, 25.0f);
+            Omaha.AddItem("steel", 0, 20.0f, 10.0f);
+            Omaha.AddItem("iron", 0, 50.0f, 20.0f);
+            Omaha.AddItem("coal", 0, 40.0f, 10.0f);
+            Omaha.AddItem("alcohol", 0, 100.0f, 25.0f);
             cities.Add("Omaha", Omaha);
-			
-			Town Welch = new Town();
-			Welch.AddItem("food", 3, 50.0f, 25.0f);
+            
+            Town Welch = new Town();
+            Welch.AddItem("food", 3, 50.0f, 25.0f);
             Welch.AddItem("water", 2, 35.0f, 10.0f);
             Welch.AddItem("guns", 5, 85.0f, 45.0f);
-			Welch.AddItem("steel", 0, 20.0f, 10.0f);
-			Welch.AddItem("iron", 0, 50.0f, 10.0f);
-			Welch.AddItem("coal", 10, 20.0f, 10.0f);
-			Welch.AddItem("alcohol", 10, 50.0f, 15.0f);
+            Welch.AddItem("steel", 0, 20.0f, 10.0f);
+            Welch.AddItem("iron", 0, 50.0f, 10.0f);
+            Welch.AddItem("coal", 10, 20.0f, 10.0f);
+            Welch.AddItem("alcohol", 10, 50.0f, 15.0f);
+            cities.Add("Welch", Welch);
         }
 
         static string UppercaseFirst(string s)
